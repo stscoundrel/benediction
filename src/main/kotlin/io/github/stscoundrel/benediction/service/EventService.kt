df361364e0a2262c4319614b3c9a6bf879b7a6d7
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2023-07-08 10:21:28 +0300

    Add queries by project name, start and end
    Closes #18

--- src/main/kotlin/io/github/stscoundrel/benediction/service/EventService.kt
+++ src/main/kotlin/io/github/stscoundrel/benediction/service/EventService.kt
@@ -3,12 +3,21 @@ package io.github.stscoundrel.benediction.service
 import io.github.stscoundrel.benediction.model.Event
 import io.github.stscoundrel.benediction.repository.EventRepository
 import org.springframework.stereotype.Service
+import java.time.Instant
 
 @Service
 class EventService(private val eventRepository: EventRepository) {
 
     fun saveEvent(event: Event): Event {
         return eventRepository.save(event)
+    }
+
+    fun getEventsByProjectNameAndTimestampRange(
+        projectName: String,
+        startTimestamp: Instant,
+        endTimestamp: Instant
+    ): List<Event> {
+        return eventRepository.findByProjectNameAndTimestampBetween(projectName, startTimestamp, endTimestamp)
     }
 
     fun getAllEvents(): List<Event> {
